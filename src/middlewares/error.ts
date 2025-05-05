768872810cc8c1094116aa995f600deb8ff7c269
Alexey S <blueseasailordeveloper@gmail.com>
2025-05-05 13:12:04 +0300

    Sprint-1: Fix lint

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,7 +1,8 @@
-import { Response, Request, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
+
 import { BaseError } from '../helper/errors';
 
-const errorHandler = (err: BaseError, _req: Request, res: Response, next: NextFunction) => {
+const errorHandler = (err: BaseError, _req: Request, res: Response, _next: NextFunction) => {
   const { message, statusCode = 500 } = err;
   res.status(statusCode);
   res.send({ message });
