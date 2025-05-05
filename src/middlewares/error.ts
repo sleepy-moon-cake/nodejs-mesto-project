8c9e9e8416fd5df67d8116233a746abcede3e038
Alexey S <blueseasailordeveloper@gmail.com>
2025-05-05 13:40:16 +0300

    Sprint-1: Update response of main error handler

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -5,6 +5,6 @@ import { BaseError } from '../helper/errors';
 const errorHandler = (err: BaseError, _req: Request, res: Response, _next: NextFunction) => {
   const { message, statusCode = 500 } = err;
   res.status(statusCode);
-  res.send({ message });
+  res.send({ statusCode, error: 'Not Found', message });
 };
 export default errorHandler;
